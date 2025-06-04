9e3e9fadc4aec60d40ca9a07e5fb1b8044a4f3f7
Sourav <205398826+morningstarxcdcode@users.noreply.github.com>
2025-06-04 06:48:06 +0000

    Add webcam availability check and improve error handling

--- virtualpainter.py
+++ virtualpainter.py
@@ -82,104 +82,193 @@ import numpy as np
 import time
 import os
 import HandTrackingModule as htm
+import pygame
+
+# Initialize pygame mixer for sound effects with dummy audio driver fallback
+import os
+os.environ['SDL_AUDIODRIVER'] = 'dummy'
+pygame.mixer.init()
+
+# Placeholder sound effects using simple beep sounds
+def play_beep():
+    duration = 100  # milliseconds
+    freq = 440  # Hz
+    try:
+        import winsound
+        winsound.Beep(freq, duration)
+    except ImportError:
+        # For non-Windows, use pygame beep alternative
+        pygame.mixer.Sound(buffer=pygame.sndarray.make_sound(
+            pygame.sndarray.array([4096 * (i % 2 * 2 - 1) for i in range(44100)]))).play()
+
+class DummySound:
+    def play(self):
+        play_beep()
+
+sound_color_change = DummySound()
+sound_draw_start = DummySound()
+sound_draw_stop = DummySound()
+sound_clear = DummySound()
+sound_save = DummySound()
 
 brushThickness = 15
 eraserThickness = 100
 
 folderPath = "Header"
 myList = os.listdir(folderPath)
-print(myList)
 overlayList = []
 for imPath in myList:
     image = cv2.imread(f'{folderPath}/{imPath}')
     image = cv2.resize(image, (640, 126))
     overlayList.append(image)
-print(len(overlayList))
 header = overlayList[0]
-#print(overlayList[0])
+
 drawColor = (255, 0, 255)
 
 cap = cv2.VideoCapture(0)
-print(cap.get(3))
-print(cap.get(4))
 cap.set(3, 1280)
 cap.set(4, 720)
-print(cap.get(3))
-print(cap.get(4))
 
-detector = htm.handDetector(detectionCon=1,maxHands=1)
+# Check if webcam opened successfully
+if not cap.isOpened():
+    print("Error: Could not open webcam. Please ensure a webcam is connected and accessible.")
+    exit(1)
+
+detector = htm.handDetector(detectionCon=1, maxHands=2)
 
 xp, yp = 0, 0
 imgCanvas = np.zeros((480, 640, 3), np.uint8)
 
-while True:
+# Variables for undo functionality
+undo_stack = []
 
-    # 1. Import image
+# Brush size options
+brush_sizes = [5, 10, 15, 20, 25]
+current_brush_index = 2  # default brush size index
+
+def save_canvas(img):
+    filename = f'drawing_{int(time.time())}.png'
+    cv2.imwrite(filename, img)
+    sound_save.play()
+    print(f"Canvas saved as {filename}")
+
+def clear_canvas():
+    global imgCanvas, undo_stack
+    undo_stack.append(imgCanvas.copy())
+    imgCanvas = np.zeros((480, 640, 3), np.uint8)
+    sound_clear.play()
+
+def undo():
+    global imgCanvas, undo_stack
+    if undo_stack:
+        imgCanvas = undo_stack.pop()
+        print("Undo performed")
+
+def draw_buttons(img):
+    # Draw clear button
+    cv2.rectangle(img, (10, 10), (110, 60), (0, 0, 255), -1)
+    cv2.putText(img, 'Clear', (20, 45), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+
+    # Draw save button
+    cv2.rectangle(img, (120, 10), (220, 60), (0, 255, 0), -1)
+    cv2.putText(img, 'Save', (140, 45), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+
+    # Draw undo button
+    cv2.rectangle(img, (230, 10), (330, 60), (255, 0, 0), -1)
+    cv2.putText(img, 'Undo', (250, 45), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+
+    # Draw brush size buttons
+    for i, size in enumerate(brush_sizes):
+        color = (255, 255, 255)
+        if i == current_brush_index:
+            color = (0, 255, 255)
+        cv2.circle(img, (400 + i*50, 35), size, color, -1)
+
+def check_button_click(x, y):
+    global drawColor, header, current_brush_index
+    if 10 < x < 110 and 10 < y < 60:
+        clear_canvas()
+        return True
+    elif 120 < x < 220 and 10 < y < 60:
+        save_canvas(imgCanvas)
+        return True
+    elif 230 < x < 330 and 10 < y < 60:
+        undo()
+        return True
+    elif 375 < x < 625 and 10 < y < 60:
+        for i in range(len(brush_sizes)):
+            if 400 + i*50 - brush_sizes[i] < x < 400 + i*50 + brush_sizes[i]:
+                current_brush_index = i
+                sound_color_change.play()
+                return True
+    return False
+
+drawing = False
+
+while True:
     success, img = cap.read()
-    
     img = cv2.flip(img, 1)
 
-    # # 2. Find Hand Landmarks
     img = detector.findHands(img)
     lmList = detector.findPosition(img, draw=False)
 
-    if len(lmList) != 0:
+    draw_buttons(img)
 
-         # print(lmList)
- 
-         # tip of index and middle fingers
-         x1, y1 = lmList[8][1:]
-         x2, y2 = lmList[12][1:]
- 
-         # 3. Check which fingers are up
-         fingers = detector.fingersUp()
-         #print(fingers)
- 
-        # 4. If Selection Mode - Two finger are up
-         if fingers[1] and fingers[2]:
-            # xp, yp = 0, 0
-            print("Selection Mode")
-            # Checking for the click
+    if len(lmList) != 0:
+        x1, y1 = lmList[8][1:]
+        x2, y2 = lmList[12][1:]
+
+        fingers = detector.fingersUp()
+
+        if fingers[1] and fingers[2]:
+            drawing = False
             if y1 < 126:
                 if 100 < x1 < 220:
                     header = overlayList[0]
                     drawColor = (255, 255, 0)
+                    sound_color_change.play()
                 elif 250 < x1 < 350:
                     header = overlayList[1]
-                    drawColor = (135,206,235)
+                    drawColor = (135, 206, 235)
+                    sound_color_change.play()
                 elif 380 < x1 < 500:
                     header = overlayList[2]
                     drawColor = (0, 255, 0)
+                    sound_color_change.play()
                 elif 520 < x1 < 640:
                     header = overlayList[3]
                     drawColor = (0, 0, 0)
+                    sound_color_change.play()
             cv2.rectangle(img, (x1, y1 - 25), (x2, y2 + 25), drawColor, cv2.FILLED)
- 
-        # 5. If Drawing Mode - Index finger is up
-         if fingers[1] and fingers[2] == False:
-            cv2.circle(img, (x1, y1), 15, drawColor, cv2.FILLED)
-            print("Drawing Mode")
-            if xp == 0 and yp == 0:
+
+        elif fingers[1] and not fingers[2]:
+            if check_button_click(x1, y1):
+                pass
+            else:
+                cv2.circle(img, (x1, y1), brush_sizes[current_brush_index], drawColor, cv2.FILLED)
+                if not drawing:
+                    sound_draw_start.play()
+                    drawing = True
+                    xp, yp = x1, y1
+                    undo_stack.append(imgCanvas.copy())
+                cv2.line(img, (xp, yp), (x1, y1), drawColor, brush_sizes[current_brush_index])
+                cv2.line(imgCanvas, (xp, yp), (x1, y1), drawColor, brush_sizes[current_brush_index])
                 xp, yp = x1, y1
 
-            cv2.line(img, (xp, yp), (x1, y1), drawColor, brushThickness)
-            cv2.line(imgCanvas, (xp, yp), (x1, y1), drawColor, brushThickness)
+        else:
+            if drawing:
+                sound_draw_stop.play()
+            drawing = False
 
+        if all(x >= 1 for x in fingers):
+            clear_canvas()
 
-            xp, yp = x1, y1
-
-        # Clear Canvas when all fingers are up
-         if all (x >= 1 for x in fingers):
-            imgCanvas = np.zeros((480, 640, 3), np.uint8)
-
-    # Setting the header image
     img[0:126, 0:640] = header
     imgCanvas = cv2.resize(imgCanvas, (img.shape[1], img.shape[0]))
-    img = cv2.addWeighted(img,0.5,imgCanvas,0.5,0)
+    img = cv2.addWeighted(img, 0.5, imgCanvas, 0.5, 0)
+
     cv2.imshow("Image", img)
-    #cv2.imshow("Canvas", imgCanvas)
-    #cv2.imshow("Inv", imgInv)
     cv2.waitKey(1)
-    
-cap.release()   # stop capturing the image/video
+
+cap.release()
 cv2.destroyAllWindows()
